8d3d706bfff44a0cc24aff9f72abe95bec72632f
Noah van der Aa <ndvdaa@gmail.com>
2025-07-28 18:34:39 +0200

    stupid stupid stupid stupid

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
         maven("https://repo.papermc.io/repository/maven-public/")
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "HealthHider"
